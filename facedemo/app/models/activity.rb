d6fe2481a57d2fb7838925be49d20c32960191ff
kumarrus <rushab3737@gmail.com>
2014-01-25 04:04:04 -0500

    Modify controller to reduce frontend load

--- facedemo/app/models/activity.rb
+++ facedemo/app/models/activity.rb
@@ -28,8 +28,9 @@ class Activity < ActiveRecord::Base
   	return Activity.where(:user_id => user_id, :tag_id => tag_id).first.state
   end
 
-  def self.toggle_state(description, user_id, state)
-    a = Activity.where(:user_id => user_id, :desc => description).first
+  def self.toggle_state(activity_id, user_id, state)
+    #a = Activity.where(:user_id => user_id, :id => activity_id).first
+    a = Activity.find(activity_id)
     if a.blank?
     	return "failure"
     a.state = state
@@ -42,21 +43,22 @@ class Activity < ActiveRecord::Base
   def self.save_activity!(description,user_id)
     hashtags = description.scan(/#\S+/) # grabs all hashtags
     if hashtags.size != 1
-      return "failure"
+      return nil
     end
     t = Tag.find_or_create_tag(hashtags.first.to_s.downcase)
     # Create new activity or modify existing one
   	a = Activity.where(:user_id => user_id, :tag_id => t.id).first
-  	if a.blank?
-  	  a = Activity.new
+  	if a.present?
+  	  return nil
   	end
+  	a = Activity.new
   	a.desc = description
   	a.state = "Wish"
   	a.tag_id = t.id
   	a.user_id = user_id
   	a.save
   	#Tag.change_score_up(t.id) # increment the tag score
-  	return "success"
+  	return a
   end
 
 end
