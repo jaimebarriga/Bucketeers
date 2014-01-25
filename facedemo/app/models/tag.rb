aacb6091cfa305defca4187e8b0da5991187d1af
pranayk <pran262@gmail.com>
2014-01-25 00:22:49 -0500

    Adding tag.rb model functions

--- facedemo/app/models/tag.rb
+++ facedemo/app/models/tag.rb
@@ -3,14 +3,24 @@ class Tag < ActiveRecord::Base
   has_many :users, through: :activities
   attr_accessible :name, :score
 
-  # Get the name of the tag with the tag
-  def get_name(tag_id)
 
+  # This function is calld to increment score by activity if a activity is created.
+  def self.change_score_up(tag_id)
+  	tag = Tag.find_by_id(tag_id)
+  	tag.score += 1
+  	tag.save
+  	return tag.score
   end
 
-  # This function is calld by activity if a activity is created or deleted.
-  def change_score(tag_id)
-
+  # This function is calld to increment score by activity if a activity is deleted.
+  def self.change_score_down(tag_id)
+  	tag = Tag.find_by_id(tag_id)
+  	if (tag.score = 0)
+  		return tag.score
+  	end
+  	tag.score -= 1
+  	tag.save
+  	return tag.score
   end
 
   # This function will be used for our "middle" column
@@ -35,11 +45,32 @@ class Tag < ActiveRecord::Base
   #     activity: "description3 #mountainclimbing boo"
   #   }
   # ]
-  def get_all_activities(tag_id, user_id)
+  def self.get_all_activities(tag_id, user_id)
+  	activities = Activity.find(:all, :conditions => {:tag_id => tag_id}).sort_by { |activity| activity[:user_id] }
+  	user = User.find_by_id(user_id)
+  	user_friends = user.friends
+  	response = []
 
+  	activities.each do |activity|
+  		
+  		if activity.user_id == user.id
+  			# Adding current users info and his activity info
+  			response << {user_id: user.uid, user_name: user.name, description: activity.desc}
+  		end
+
+  		puts activity.desc
+  		tag_matched_user = activity.user
+  		is_match_with_friends = user_friends.detect {|friend| friend["id"] == tag_matched_user.uid }
+  		unless is_match_with_friends.blank?
+  			# Adding all the friend users info and their activity info
+  			response << {user_id: tag_matched_user.uid, user_name: tag_matched_user.name, description: activity.desc}
+  		end
+  	end
+
+  	return response
   end
 
   def add_tag(tag_name)
-
+  	return Tag.find_or_create_by(name: tag_name)
   end
 end
